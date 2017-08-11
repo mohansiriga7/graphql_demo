412576d116211390344533d6b0a40fae18d76d34
mohansiriga7 <mohan0707>
2017-08-11 17:25:36 +0530

    add project model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170811114554) do
+ActiveRecord::Schema.define(version: 20170811115234) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "projects", ["user_id"], name: "index_projects_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
